50dc89a85400ffbe42c2f31cc0be9b0c2e526fd0
Atmoslayer <ddsomdim@gmail.com>
2023-09-29 15:47:23 +0300

    Fix rollbar token usage

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+stty -echo
 cd /opt/star-burger
 git pull
 source venv/bin/activate
@@ -12,10 +13,10 @@ npm ci -dev
 deactivate
 systemctl restart star-burger.service
 commit_version=$(git rev-parse --verify HEAD)
-echo rollbar_token =`sed -n 's/^ROLLBAR_TOKEN=\(.*\)/\1/p' < .env`
+echo rollbar_token=`sed -n 's/^ROLLBAR_TOKEN=\(.*\)/\1/p' < .env`
 curl --request POST \
      --url https://api.rollbar.com/api/1/deploy \
-     --header "${rollbar_token}" \
+     --header "X-Rollbar-Access-Token: $rollbar_token" \
      --header 'accept: application/json' \
      --header 'content-type: application/json' \
      --data '
@@ -25,4 +26,5 @@ curl --request POST \
         "rollbar_username": "ddsomdim"
 }
 '
+stty echo
 echo $'\nSuccessful deployment'
