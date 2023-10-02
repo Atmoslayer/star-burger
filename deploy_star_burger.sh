83bb28a938e85eaf5008f820eb316f3214fc52d3
Atmoslayer <ddsomdim@gmail.com>
2023-10-02 23:58:57 +0300

    Add no input flag for manage.py commands in deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-usage="$(basename "$0") [-h] -- deploy script for star-burger project.
-where:
-    -h  use ./deploy_star_burger.sh to rin script"
 set -e
 cd /opt/star-burger
 git pull
 source venv/bin/activate
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py load_locations
 npm ci -dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
