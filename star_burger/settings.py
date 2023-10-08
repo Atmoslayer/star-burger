36aacce2a403b47f3fb58affde55fa1b0ad65872
Atmoslayer <ddsomdim@gmail.com>
2023-10-08 20:03:34 +0300

    Set up sqlite3 db to test docker

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -95,7 +95,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': DATABASE
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
