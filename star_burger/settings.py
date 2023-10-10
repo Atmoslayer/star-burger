cf9a53f586c4a1f3e1325b98a0d4bce8ec5fa02e
Atmoslayer <ddsomdim@gmail.com>
2023-10-10 15:54:28 +0300

    Fix sqlite connection

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -95,10 +95,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
